375417258a2c97d66786bf6a4f666c357489533f
AndersonM <andersonmarquesweb@gmail.com>
2023-05-21 16:29:49 -0500

    new page: Dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Dashboard, { dashboardLoader } from './pages/Dashboard';
+import Error from './pages/Error';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Dashboard />,
+    loader: dashboardLoader,
+    errorElement: <Error />,
+  },
+]);
+
 function App() {
   return (
-    <div>
-      <h1>App</h1>
+    <div className="App">
+      <RouterProvider router={router} />
     </div>
   );
 }
---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,19 @@
+//rrd imports
+import { useLoaderData } from 'react-router-dom';
+
+//helper functions
+import { fetchData } from '../helpers';
+
+//loader
+export function dashboardLoader() {
+  const userName = fetchData('userName');
+  return { userName };
+}
+
+const Dashboard = () => {
+  const { userName } = useLoaderData();
+
+  return <div>Dashboard</div>;
+};
+
+export default Dashboard;
