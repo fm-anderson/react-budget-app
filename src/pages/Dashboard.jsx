0a86df3f5e1c20b179d416a677d52e658bbd3e5b
AndersonM <andersonmarquesweb@gmail.com>
2023-05-21 18:20:12 -0500

    new component: AddBudgetForm

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -9,11 +9,13 @@ import { fetchData } from '../helpers';
 
 //components
 import Intro from '../components/Intro';
+import AddBudgetForm from '../components/AddBudgetForm';
 
 //loader
 export function dashboardLoader() {
   const userName = fetchData('userName');
-  return { userName };
+  const budgets = fetchData('budgets');
+  return { userName, budgets };
 }
 
 //action
@@ -29,9 +31,29 @@ export async function dashboardAction({ request }) {
 }
 
 const Dashboard = () => {
-  const { userName } = useLoaderData();
+  const { userName, budgets } = useLoaderData();
 
-  return <>{userName ? <p>{userName}</p> : <Intro />}</>;
+  return (
+    <>
+      {userName ? (
+        <div className="dashboard">
+          <h1>
+            Welcome back, <span className="accent">{userName}</span>
+          </h1>
+          <div className="grid-sm">
+            {/* {budgets ? () : ()} */}
+            <div className="grid-lg">
+              <div className="flex-lg">
+                <AddBudgetForm />
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <Intro />
+      )}
+    </>
+  );
 };
 
 export default Dashboard;
