762df3f38f401b293b1e3e03da7ea4b5953223d2
AndersonM <andersonmarquesweb@gmail.com>
2023-05-22 17:07:57 -0500

    fix: BudgetPage delete action

--- src/components/BudgetItem.jsx
+++ src/components/BudgetItem.jsx
@@ -1,11 +1,14 @@
 // helper functions
+import { Form, Link } from 'react-router-dom';
 import {
   calculateSpentByBudget,
   formatCurrency,
   formatPercentage,
 } from '../helpers';
+import { BanknotesIcon } from '@heroicons/react/24/outline';
+import { TrashIcon } from '@heroicons/react/24/solid';
 
-const BudgetItem = ({ budget }) => {
+const BudgetItem = ({ budget, showDelete = false }) => {
   const { id, name, amount, color } = budget;
   const spent = calculateSpentByBudget(id);
 
@@ -27,6 +30,35 @@ const BudgetItem = ({ budget }) => {
         <small>{formatCurrency(spent)} spent</small>
         <small>{formatCurrency(amount - spent)} remaining</small>
       </div>
+      {showDelete ? (
+        <div className="flex-sm">
+          <Form
+            method="post"
+            action="delete"
+            onSubmit={(event) => {
+              if (
+                !confirm(
+                  'Are you sure you want to permanently delete this budget?'
+                )
+              ) {
+                event.preventDefault();
+              }
+            }}
+          >
+            <button className="btn">
+              <span>Delete Budget</span>
+              <TrashIcon width={20} />
+            </button>
+          </Form>
+        </div>
+      ) : (
+        <div className="flex-sm">
+          <Link to={`/budget/${id}`} className="btn">
+            <span>View Details</span>
+            <BanknotesIcon width={20} />
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
