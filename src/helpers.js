c408783cbd8a384a40c07449511456cf4f32748e
AndersonM <andersonmarquesweb@gmail.com>
2023-05-22 14:38:03 -0500

    fix: delete user data from local storage

--- src/helpers.js
+++ src/helpers.js
@@ -1,5 +1,5 @@
 export const waait = () =>
-  new Promise((res) => setTimeout(res, Math.random() * 1000));
+  new Promise((res) => setTimeout(res, Math.random() * 800));
 
 const generateRandomColor = () => {
   const existingBudgetLength = fetchData('budgets')?.length ?? 0;
