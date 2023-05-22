1a0ef286820fd3a76ff39f7e53a3a7c408e5f419
AndersonM <andersonmarquesweb@gmail.com>
2023-05-22 16:36:07 -0500

    new page: BudgetPage

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ import ExpensesPage, {
   expensesAction,
   expensesLoader,
 } from './pages/ExpensesPage';
+import BudgetPage, { budgetAction, budgetLoader } from './pages/BudgetPage';
 
 const router = createBrowserRouter([
   {
@@ -33,10 +34,18 @@ const router = createBrowserRouter([
         errorElement: <Error />,
       },
       {
+        path: 'budget/:id',
+        element: <BudgetPage />,
+        loader: budgetLoader,
+        action: budgetAction,
+        errorElement: <Error />,
+      },
+      {
         path: 'expenses',
         element: <ExpensesPage />,
         loader: expensesLoader,
         action: expensesAction,
+        errorElement: <Error />,
       },
       {
         path: 'logout',
